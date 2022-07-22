5be112d9f8243fc20b792a1255de84584428e1c6
Lil_Tiger <anjm888@126.com>
2022-07-22 11:05:24 +0800

    update.

--- clean.py
+++ clean.py
@@ -1,12 +1,8 @@
 # 由于使用的SCOP中包含TAPE远程同源检测fold_level的测试集 故在最开始clean掉这些数据 再划分训练集和测试集
 import pandas as pd
-import csv
-import numpy as np
 import tqdm
 import json
 
-csv_file = open('protein.csv')
-reader = csv.reader(csv_file)
 # read_csv方法默认会在开头加入新的unnamed列 设置index_col=0可以避免此现象
 df = pd.read_csv('protein.csv', index_col=0)
 # 重复的sequence数量和列表
@@ -27,4 +23,4 @@ with open("remote_homology_test_fold_holdout.json", 'r') as fp:
                 n += 1
 
 print(n)
-df.to_csv('protein_cleaned.csv')
+df.to_csv('protein_cleaned.csv', index=None)
